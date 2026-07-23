49dd8de99273b6a1f5b54bc90c64c7b4b5865d75
kamleshbhure <student-01-612cf8ef90b4@qwiklabs.net>
2026-07-23 19:20:40 +0000

    first version

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+
 func blueHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
 	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
